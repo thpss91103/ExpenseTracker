a596027a957af7ed8c7b8d7908f5a53dfd5dd629
thpss91103 <a0975142932@gmail.com>
2023-05-06 17:53:01 +0800

    feat: add routes

---
+++ routes/index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const router = express.Router()
+const home = require('./modules/home')
+const expenses = require('./modules/expenses')
+const category = require("./modules/category")
+const users = require('./modules/users')
+const { authenticator } = require('../middleware/auth')
+
+router.use('/expenses', authenticator, expenses)
+router.use('/users', users)
+router.use("/category", authenticator, category)
+router.use('/', authenticator, home)
+
+module.exports = router
